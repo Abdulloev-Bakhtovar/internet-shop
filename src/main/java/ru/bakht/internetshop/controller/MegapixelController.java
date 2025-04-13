730e1fd2305c4c2c9bbc44d3483c51c5ee7c78d1
BAKHTOVAR <bakhtovar.abdulloev.03@mail.ru>
2025-04-13 20:50:41 +0500

    fix: заменить TypeDto на MegapixelDto в MegapixelController

--- src/main/java/ru/bakht/internetshop/controller/MegapixelController.java
+++ src/main/java/ru/bakht/internetshop/controller/MegapixelController.java
@@ -2,14 +2,14 @@ package ru.bakht.internetshop.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.bakht.internetshop.model.dto.TypeDto;
+import ru.bakht.internetshop.model.dto.MegapixelDto;
 import ru.bakht.internetshop.service.BaseProductFeatureService;
 
 @RestController
 @RequestMapping("/cameras/megapixel")
-public class MegapixelController extends AbstractProductFeatureController<TypeDto> {
+public class MegapixelController extends AbstractProductFeatureController<MegapixelDto> {
 
-    public MegapixelController(BaseProductFeatureService<TypeDto> service) {
+    public MegapixelController(BaseProductFeatureService<MegapixelDto> service) {
         super(service);
     }
 }
