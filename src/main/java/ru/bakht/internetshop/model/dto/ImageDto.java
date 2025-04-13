41d0e691a07e917c0e2623c91a1579271ce4a3b2
BAKHTOVAR <bakhtovar.abdulloev.03@mail.ru>
2025-04-13 19:40:27 +0500

    refactor: обновлены импорты в соответствии с новой структурой пакетов

--- src/main/java/ru/bakht/internetshop/model/dto/ImageDto.java
+++ src/main/java/ru/bakht/internetshop/model/dto/ImageDto.java
@@ -2,6 +2,7 @@ package ru.bakht.internetshop.model.dto;
 
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import ru.bakht.internetshop.auth.model.dto.BaseDto;
 
 @SuperBuilder
 @NoArgsConstructor
--- src/main/java/ru/bakht/internetshop/model/dto/OrderDto.java
+++ src/main/java/ru/bakht/internetshop/model/dto/OrderDto.java
@@ -3,6 +3,8 @@ package ru.bakht.internetshop.model.dto;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
+import ru.bakht.internetshop.auth.model.dto.BaseDto;
+import ru.bakht.internetshop.auth.model.dto.UserInfoDto;
 
 import java.time.Instant;
 import java.util.List;
