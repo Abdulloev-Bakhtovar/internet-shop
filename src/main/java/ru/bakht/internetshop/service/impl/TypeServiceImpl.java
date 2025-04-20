c4cd6a63935772f39322d346355e721c8a37ccf7
BAKHTOVAR <bakhtovar.abdulloev.03@mail.ru>
2025-04-20 13:14:11 +0500

    бновлен TypeServiceImpl для работы с новым репозиторием

--- src/main/java/ru/bakht/internetshop/service/impl/TypeServiceImpl.java
+++ src/main/java/ru/bakht/internetshop/service/impl/TypeServiceImpl.java
@@ -8,7 +8,7 @@ import ru.bakht.internetshop.exception.AppException;
 import ru.bakht.internetshop.mapper.BaseProductFeatureMapper;
 import ru.bakht.internetshop.model.Type;
 import ru.bakht.internetshop.model.dto.TypeDto;
-import ru.bakht.internetshop.repository.TypeRepository;
+import ru.bakht.internetshop.repository.TypeRepo;
 import ru.bakht.internetshop.service.BaseProductFeatureService;
 
 import java.util.List;
@@ -19,13 +19,13 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class TypeServiceImpl implements BaseProductFeatureService<TypeDto> {
 
-    private final TypeRepository typeRepository;
+    private final TypeRepo typeRepo;
     private final BaseProductFeatureMapper<TypeDto, Type> typeMapper;
 
     @Override
     @Transactional(readOnly = true)
     public List<TypeDto> getAll() {
-        return typeRepository.findAll().stream()
+        return typeRepo.findAll().stream()
                 .map(typeMapper::toDto)
                 .toList();
     }
@@ -33,14 +33,14 @@ public class TypeServiceImpl implements BaseProductFeatureService<TypeDto> {
     @Override
     public void create(TypeDto typeDto) {
         var type = typeMapper.toEntity(typeDto);
-        typeRepository.save(type);
+        typeRepo.save(type);
     }
 
     @Override
     public void changeVisibility(UUID id, boolean isVisible) {
-        var type = typeRepository.findById(id)
+        var type = typeRepo.findById(id)
                 .orElseThrow(() -> new AppException("Type not found with ID: " + id, HttpStatus.NOT_FOUND));
         type.setIsVisible(isVisible);
-        typeRepository.save(type);
+        typeRepo.save(type);
     }
 }
